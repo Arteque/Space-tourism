aa25e4e8502fcfef68a7a17b26b2136dc3fdcd05
Factum <art3que@gmail.com>
2024-02-16 00:28:25 +0100

    subpage

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //Init function
 burgerClickEvent()
-
+sliderNavContainer()
 // Navigation and Burger Menu behavior on Click for mobile
 // Functions
 function checkItem(item){
     if(!item) return
 }
+
 
 function burgerClickEvent(){
     //check if burger menu is loaded
@@ -21,4 +22,32 @@ function burgerClickEvent(){
             burgerContainer.classList.toggle("show")
             mainNavContainer.classList.toggle("show")
           })
+}
+
+//Subpage 
+function sliderNavContainer(){
+  //Check if the nav exist
+  checkItem(document.querySelector(".slider-nav-container"))
+  
+
+  sliderNav = document.querySelector(".slider-nav-container")
+  sliderNavItems = [...sliderNav.querySelectorAll("li")]
+  sliderMediaItems = document.querySelectorAll("#hero-section .content .slider-media-container .media")
+  sliderTextItems = document.querySelectorAll("#hero-section .content .slider-text-container .slider-text")
+  sliderNavItems.forEach((item,index) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault()
+      if(item.classList.value.includes("current")) return
+
+      for(let i = 0; i < sliderNavItems.length; i++){
+        sliderNavItems[i].classList.remove("current")
+        sliderMediaItems[i].classList.remove("current")
+        sliderTextItems[i].classList.remove("current")
+      }
+
+      item.classList.add("current")
+      sliderMediaItems[index].classList.add("current")
+      sliderTextItems[index].classList.add("current")
+    })
+  })
 }
