aeb903fcf016c53bba10d5c2a05c66b06954b851
Factum <art3que@gmail.com>
2024-02-17 20:12:02 +0100

    start page nav bar

--- Script/app.js
+++ Script/app.js
@@ -1,6 +1,6 @@
 //Init function
 burgerClickEvent()
-sliderNavContainer()
+// sliderNavContainer()
 // Navigation and Burger Menu behavior on Click for mobile
 // Functions
 function checkItem(item){
@@ -12,8 +12,8 @@ function burgerClickEvent(){
     //check if burger menu is loaded
     checkItem(document.querySelector(".burger-container"))
 
-    const burgerContainer = document.querySelector(".burger-container"),
-          mainNavContainer = document.querySelector("#main-nav-container")
+    const burgerContainer = document.querySelector(".burger-container")
+    const mainNavContainer = document.querySelector("#main-nav-container")
 
           burgerContainer.addEventListener("click", (e) => {
             //just in case the click event behave is inexpected -> prevent the default
